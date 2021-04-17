d3d2013b04693a41c2a92087192f9821fb3ee289
zamir <zamir.arif01@gmail.com>
2021-04-17 22:20:58 +0800

    Add project files.

---
+++ Array/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strNamaHari(6) As String
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+
+        Dim a As Integer
+        For a = 0 To 6
+            ListBox1.Items.Add(strNamaHari(a))
+        Next
+
+    End Sub
+End Class
